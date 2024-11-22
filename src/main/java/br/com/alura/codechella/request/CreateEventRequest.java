d5186d15091617964c3ced82d229ffff8b0987e8
Matteus Moreno <matteus_moreno@live.com>
2024-11-22 16:22:19 -0300

    adicionando a validação da inclusão de data no request da criação de event

--- src/main/java/br/com/alura/codechella/request/CreateEventRequest.java
+++ src/main/java/br/com/alura/codechella/request/CreateEventRequest.java
@@ -4,10 +4,14 @@ import br.com.alura.codechella.constant.EventType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDate;
+
 public record CreateEventRequest(
         @NotNull(message = "Type is required")
         EventType type,
         @NotBlank(message = "Name is required")
         String name,
+        @NotNull(message = "Date is required")
+        LocalDate date,
         String description) {
 }
